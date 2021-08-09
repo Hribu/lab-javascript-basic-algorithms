6d2afd0b693060e54e6b63ffe5dd86a21ee5253d
FlavianAlbert <flahribu@gmail.com>
2021-08-10 01:15:42 +0200

    done_v1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,26 @@
 // Iteration 1: Names and Input
-let hacker1 = "xxxx"
-console.log(`The driver's name is ${hacker1}`);
-
-let hacker2 = "YYYY"
+const hacker1 = "John"
+console.log(`The driver's name is ${hacker1}`)
+const hacker2 = "Anonymous"
 console.log(`The navigator's name is ${hacker2}`)
 
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+  }else if(hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  }else{
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+  }
 
-// Iteration 2: Conditionals 
-/*
-      if (hacker1.length >= hacker2.length) {  
-        console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)  
-        } else if (hacker2.length >= hacker1.length) {    
-        console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-       } else (hacker1.length && hacker2.length) {
-           console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
-       }   
-       
-       SyntaxError: Unexpected token '{' */
-    
+  
+
 // Iteration 3: Loops
-/*
-for(let i = 0; i <= hacker1.length; i++) {
-  result += hacker1[i].toUpperCase() + " ";
-  } 
-  console.log(result); */
-/* result is not defined */
+ let uperCaseDriver=""
+for(let i=0; i< hacker1.length; i++){
+    uperCaseDriver += hacker1[i].toUpperCase() + " " 
+}
+console.log(uperCaseDriver)
+
+console.log(hacker2.split("").reverse().join(""))
 
